c273d68cba2ef1a0c340efbae660bfe6053a2c8a
BarneyLoosemore <biscuitfull@gmail.com>
2018-10-08 16:15:59 +0100

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
+require 'sinatra'
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Barney"
+    # return 200
+  end
+
+  get '/hometown' do
+    "My hometown is London"
+    # return 200
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Talk Talk - Ascension Day"
+    # return 200
+  end
+
 end
